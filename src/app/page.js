487c4deaa19a168b51c0073557d4106a8aa01c6a
codewithzaman <naderzaman50@gmail.com>
2024-05-19 16:56:11 +0500

    Update page.js
    Navigation using Link

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
       <h1>Welcome to Nextjs course 2024</h1>
+      <Link href={"/products"}>Navigate to products page</Link>
+      <Link href={"/account"}>Navigate to account page</Link>
     </main>
   );
 }
